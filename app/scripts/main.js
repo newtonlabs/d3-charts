bfbc6d918c7ec74929c56533419531bcc47df94e
Thomas Newton <thomas_newton@mckinsey.com>
2013-08-22 11:42:54 -0400

    data parsing in heatmap

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,8 +25,41 @@ d3.tsv("constants/data.tsv", function(error, data) {
 
 
 var heatmapChart = d3.charts.heatmap();
+var populate = function(category, obj) {
+  var data = {
+    xaxis: obj.xAxis,
+    yaxis: obj.yAxis,
+    color: obj.color,
+    trend: obj.trend,
+    value: obj.value,
+    target: obj.target,
+    id: obj.ID
+
+  }
+  category.data.push(data);
+};
+
 d3.csv("constants/heatmap_data.csv", function(error, data) {
   console.log(data);
-   d3.select("#heatmap").datum(data).call(heatmapChart);
+
+  // Maintain order from the JSON object
+  var scrubbed = _.reduce(data, function(memo, obj) {
+
+    var category = _.find(memo, function(d) {return d.name == obj.category});
+
+    if (category) {
+      populate(category, obj)
+    }
+    else {
+      category = {};
+      category.name = obj.category;
+      category.data = [];
+      memo.push(category);
+    }
+
+    return memo
+  },[]);
+
+  d3.select("#heatmap").datum(scrubbed).call(heatmapChart);
 });
 
