028f13cee4c972ce5dc62caa670838098f2d4511
Thomas Newton <thomas_newton@mckinsey.com>
2013-08-30 13:51:46 -0400

    first pass at filters for timeseries

--- app/scripts/drivers/timeseries.js
+++ app/scripts/drivers/timeseries.js
@@ -1,43 +1,38 @@
 
 var timeseriesChart = d3.charts.timeseries();
-
 var parseDate = d3.time.format("%Y%m%d").parse;
 
-d3.tsv("data/timeseries_old_data.tsv", function(error, data) {
-  var series = d3.keys(data[0]).filter(function(key) { return key !== "date"; })
-  console.log(series);
+var aggregate = function(data, category, stack) {
+  var test = [];
 
-  data.forEach(function(d) {
-    d.date = parseDate(d.date)
-  })
-
-  var scrubbed = _.map(series, function(name) {
-    return {
-      series: name,
-      data: _.map(data, function(d) {
-        return {date: d.date, value: +d[name]};
+  _.each(
+    _.filter(data, function(d) {
+       return (d.category == category && d.yAxis == stack);
+    }), function(d) {
+      test.push({
+        date: parseDate(d.xAxis),
+        value: +d.value
       })
-    };
-  });
-  console.log(scrubbed);
+    })
+  return test;
+}
 
- d3.select("#timeseries").datum(scrubbed).call(timeseriesChart);
+d3.csv("data/timeseries_data.csv", function(error, data) {
+
+  var series = d3.utilities.uniqueProperties(data, "category");
+  var stacks = d3.utilities.uniqueProperties(data, "yAxis");
+
+  var teamNameFilter = "Team 1" || series[0];
+  var metricFilter   = "Net Promoter Score" || stacks[0];
+
+  var datum  = aggregate(data, teamNameFilter, metricFilter);
+
+  var scrubbed = {
+    series: teamNameFilter,
+    data: datum
+  };
+
+  d3.select("#timeseries").datum([scrubbed]).call(timeseriesChart);
+
+
 });
-
-// d3.csv("data/timeseries_data.csv", function(error, data) {
-//   var series = d3.utilities.uniqueProperties(data, "category");
-
-//   var scrubbed = _.map(series, function(name) {
-//     return {
-//       series: name,
-//       data: _.map(_.filter(data, function(d) {return d.category == name;}), function(d) {
-//         return {date: parseDate(d.xAxis), value: +d.value};
-//       })
-//     };
-//   });
-//   console.log([scrubbed[0]]);
-
-
-//  d3.select("#timeseries").datum([scrubbed[0]]).call(timeseriesChart);
-
-// });
