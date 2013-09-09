7e5ef9a0d40117c674884a897602f7fa8017e570
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-09-09 11:49:52 -0400

    whitespace

--- app/scripts/drivers/barchart.js
+++ app/scripts/drivers/barchart.js
@@ -20,28 +20,28 @@ d3.csv("data/barchart_data.csv", function(error, data) {
   };
 
   var categoryValue = function(data, category, xAxis) {
-          data = _.filter(data, function(d){ return ((d.category == category) && (d.xAxis == xAxis)); });
-          return _.reduce(data, function(memo, num){ return memo + Number(num.value); }, 0);
+    data = _.filter(data, function(d){ return ((d.category == category) && (d.xAxis == xAxis)); });
+    return _.reduce(data, function(memo, num){ return memo + Number(num.value); }, 0);
   }
 
 
-          data = _.filter(data, function(d){ return ((d.yAxis == 'Net Promoter Score') && (d.category == 'January')); }); //&& (d.category == 'January')
+  data = _.filter(data, function(d){ return ((d.yAxis == 'Net Promoter Score') && (d.category == 'January')); }); //&& (d.category == 'January')
 
-          var scrubbed = [],
-            rows = d3.utilities.uniqueProperties(data, 'xAxis'),
-            categories = d3.utilities.uniqueProperties(data, 'category');
+  var scrubbed = [],
+  rows = d3.utilities.uniqueProperties(data, 'xAxis'),
+  categories = d3.utilities.uniqueProperties(data, 'category');
 
-          rows.forEach(function(r) {
-            var obj = {
-              xAxis: r,
-              yAxis: data[0].yAxis,
-              target: data[0].target
-            }
-            categories.forEach(function(c) {
-              obj[c] = categoryValue(data, c, r);
-            });
-            scrubbed.push(obj);
-          }); 
+  rows.forEach(function(r) {
+    var obj = {
+      xAxis: r,
+      yAxis: data[0].yAxis,
+      target: data[0].target
+    }
+    categories.forEach(function(c) {
+      obj[c] = categoryValue(data, c, r);
+    });
+    scrubbed.push(obj);
+  }); 
   
   d3.select("#barchart").datum(scrubbed).call(barChart);
   // console.log(barChart.svg());
