3cf26edb8c2cb37c87699de99fae398463e8029d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-09-16 08:43:09 -0400

    whitespace

--- app/scripts/library/barchart.js
+++ app/scripts/library/barchart.js
@@ -1,49 +1,49 @@
 /*jslint browser: true*/
 /*global $, jQuery, d3, _*/
 
-if (d3.charts === null || typeof(d3.charts) !== "object") { d3.charts = {}; }
+  if (d3.charts === null || typeof(d3.charts) !== "object") { d3.charts = {}; }
 
 // Based on http://bost.ocks.org/mike/chart/
 this.d3.charts.barchart = function() {
- 'use strict';
+  'use strict';
 
   var width = 600,
-    height = 400,
-    svg = {},
-    margin = { top: 50, right: 20, bottom: 0, left: 75 };
-    //color = d3.scale.category20();
+  height = 400,
+  svg = {},
+  margin = { top: 50, right: 20, bottom: 0, left: 75 };
+  //color = d3.scale.category20();
 
   function my(selection) {
 
     var chartWidth    = width  - margin.left - margin.right,
-        chartHeight   = height - margin.top  - margin.bottom;
+    chartHeight   = height - margin.top  - margin.bottom;
 
     var x0 = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .3);
+      .rangeRoundBands([0, width], .3);
 
     var x1 = d3.scale.ordinal();
 
     var y = d3.scale.linear()
-        .range([chartHeight, 0]);
+      .range([chartHeight, 0]);
 
     var xAxis = d3.svg.axis()
-        .scale(x0)
-        .orient("bottom");
+      .scale(x0)
+      .orient("bottom");
 
     var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+      .scale(y)
+      .orient("left");
 
     selection.each(function(data) {
       var groups = d3.keys(data[0]).filter(function(key) { return ((key !== "xAxis") && (key !== "yAxis") && (key !== "target") && (key !== "group")); });
 
       svg = d3.select(this).append("svg")
-          .attr("class", "barchart")  //for namespacing css
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom);
+        .attr("class", "barchart")  //for namespacing css
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
 
       var context = svg.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
       
       data.forEach(function(d) {
         d.group = groups.map(function(name) { return {name: name, value: +d[name].value, color: d[name].color}; });
@@ -53,17 +53,17 @@ this.d3.charts.barchart = function() {
       x1.domain(groups).rangeRoundBands([0, x0.rangeBand()]);
 
       var d3Min =    d3.min(data, function (d) {
-          return d3.min(d.group, function (d) {
-              return d.value;
-          });
+        return d3.min(d.group, function (d) {
+          return d.value;
+        });
       });
       if (d3Min > 0)
         d3Min = 0;
 
       var d3Max =  d3.max(data, function (d) {
-           return d3.max(d.group, function (d) {
-               return d.value;
-           });
+        return d3.max(d.group, function (d) {
+          return d.value;
+        });
       });
       var target = Number(data[0].target);
 
@@ -74,71 +74,71 @@ this.d3.charts.barchart = function() {
       
       var xAxisTransform =  chartHeight;
       if(d3Min < 0 && 0 < d3Max) {
-          xAxisTransform = chartHeight * (d3Max / (d3Max - d3Min));
+        xAxisTransform = chartHeight * (d3Max / (d3Max - d3Min));
       }                      
       
       var cat = context.selectAll(".cat")
-          .data(data)
+        .data(data)
         .enter().append("g")
-          .attr("class", "g")
-          .attr("transform", function(d) { return "translate(" + x0(d.xAxis) + ",0)"; });
+        .attr("class", "g")
+        .attr("transform", function(d) { return "translate(" + x0(d.xAxis) + ",0)"; });
 
       cat.selectAll("rect")
-          .data(function(d) { return d.group; })
+        .data(function(d) { return d.group; })
         .enter().append("rect")
-          .attr("width", x1.rangeBand())
-          .attr("x", function(d) { return x1(d.name); })
-            .attr("y", function (d) {
-                if(d.value < 0)
-                    return y(0);
-                return y(d.value);
-            })
-            .attr("height", function (d) {
-                if(d.value < 0) {
-                    return y(d.value+d3Max);
-                }
-                return chartHeight - y(d.value+d3Min);
-            })
-          .style("fill", function(d) { console.log(d); return d.color; });
+        .attr("width", x1.rangeBand())
+        .attr("x", function(d) { return x1(d.name); })
+        .attr("y", function (d) {
+          if(d.value < 0)
+            return y(0);
+          return y(d.value);
+        })
+        .attr("height", function (d) {
+          if(d.value < 0) {
+            return y(d.value+d3Max);
+          }
+          return chartHeight - y(d.value+d3Min);
+        })
+        .style("fill", function(d) { console.log(d); return d.color; });
 
       context.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + xAxisTransform + ")") // this line moves x-axis
-            .call(xAxis);
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + xAxisTransform + ")") // this line moves x-axis
+        .call(xAxis);
 
       context.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-            .append("text")
-            .attr("y", -20)
-            .style("text-anchor", "start")
-            .text(data[0].yAxis);
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("y", -20)
+        .style("text-anchor", "start")
+        .text(data[0].yAxis);
 
       var line = context.append("line")
-                  .attr("class", "target")  
-                  .attr("x1", 0)
-                  .attr("y1", y(target))
-                  .attr("x2", width)
-                  .attr("y2", y(target));
+        .attr("class", "target")  
+        .attr("x1", 0)
+        .attr("y1", y(target))
+        .attr("x2", width)
+        .attr("y2", y(target));
 
       // var legend = svg.selectAll(".legend")
-          // .data(groups.slice().reverse())
-        // .enter().append("g")
-          // .attr("class", "legend")
-          // .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      // .data(groups.slice().reverse())
+      // .enter().append("g")
+      // .attr("class", "legend")
+      // .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
       // legend.append("rect")
-          // .attr("x", width - 18)
-          // .attr("width", 18)
-          // .attr("height", 18)
-          // .style("fill", color);
+      // .attr("x", width - 18)
+      // .attr("width", 18)
+      // .attr("height", 18)
+      // .style("fill", color);
 
       // legend.append("text")
-          // .attr("x", width - 24)
-          // .attr("y", 9)
-          // .attr("dy", ".35em")
-          // .style("text-anchor", "end")
-          // .text(function(d) { return d; });
+      // .attr("x", width - 24)
+      // .attr("y", 9)
+      // .attr("dy", ".35em")
+      // .style("text-anchor", "end")
+      // .text(function(d) { return d; });
     });
 
   }
