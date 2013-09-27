091b86152efb3ae29dfc73d980f4d4035c2fe273
unknown <jfanaian@18techs.com>
2013-09-26 22:11:15 -0400

    min, concat

--- build/d3.charts.js
+++ build/d3.charts.js
@@ -113,14 +113,27 @@ this.d3.charts.barchart = function() {
         .attr("class", "x axis")
         .attr("transform", "translate(0," + xAxisTransform + ")") // this line moves x-axis
         .call(xAxis);
-
-      context.append("g")
+        
+      context.append("svg:g")
         .attr("class", "y axis")
         .call(yAxis)
+      .selectAll("g")
+        .append("svg:foreignObject")
+            .attr("width",'150px')
+            .attr("height",'40px')
+            .attr("x", -160)
+            .attr("y", -20)
+            .attr("style","text-align: right;")
+        .append("xhtml:div")
+            .html(function(schema) {return schema;});
+
+      context.selectAll(".y.axis g text").remove();
+
+      context.select(".y.axis")
         .append("text")
         .attr("y", -20)
         .style("text-anchor", "start")
-        .text(data[0].yAxis);
+        .text(data[0].yAxis);      
 
       if (hasTarget()) {
         var line = context.append("line")
@@ -426,7 +439,34 @@ this.d3.charts.heatmap = function() {
 
       heatmap.selectAll(".y.axis").data(rows).enter().append("g")
         .attr("class", "y axis");
-      heatmap.select(".y.axis").transition().call(yAxis);
+      //heatmap.select(".y.axis").transition().call(yAxis);
+      
+      heatmap.selectAll(".y.axis").remove();      
+      
+      heatmap.append("svg:g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .selectAll("g")
+        .append("svg:foreignObject")
+            .attr("width",'150px')
+            .attr("height",'40px')
+            .attr("x", -160)
+            .attr("y", -20)
+            .attr("style","text-align: right;")
+        .append("xhtml:div")
+            .html(function(schema) {return schema;});
+
+      heatmap.selectAll(".y.axis g text").remove();
+      
+      // heatmap.select(".y.axis").transition().call(yAxis)
+                // .selectAll("g")
+                // .append("svg:foreignObject")
+                    // .attr("width",'300px')
+                    // .attr("height",'50px')
+                    // .attr("x", 0)
+                    // .attr("y", 0)                    
+                // .append("xhtml:div")
+                    // .html(function(d) {return d;});      
 
     };
 
@@ -669,14 +709,21 @@ this.d3.charts.timeseries = function() {
         .attr("class", "x axis")
         .attr("transform", "translate(0," + chartHeight + ")")
         .call(xAxis);
+          
+      focus.append("svg:g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .selectAll("g")
+        .append("svg:foreignObject")
+            .attr("width",'150px')
+            .attr("height",'40px')
+            .attr("x", -160)
+            .attr("y", -20)
+            .attr("style","text-align: right;")
+        .append("xhtml:div")
+            .html(function(schema) {return schema;});
 
-      focus.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-          .append("text")
-          .attr("y", -10)
-          .style("text-anchor", "start")
-          .text(data[0].series);
+      focus.selectAll(".y.axis g text").remove();          
 
       context.selectAll("path").data(data).enter().append("path")
         .attr("class", "timeline")
