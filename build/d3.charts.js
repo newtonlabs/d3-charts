44dc863ce358e4efb21e1a5f38b96c4999390b39
Thomas Newton <thomas_newton@mckinsey.com>
2013-10-01 17:10:54 -0400

    bulds

--- build/d3.charts.js
+++ build/d3.charts.js
@@ -204,6 +204,50 @@ this.d3.charts.barchart = function() {
 if (d3.charts === null || typeof(d3.charts) !== "object") { d3.charts = {}; }
 
 // Based on http://bost.ocks.org/mike/chart/
+this.d3.charts.filter = function() {
+  'use strict';
+  function my(selection) {
+    var width = 960,
+      height = 500,
+      svg = {};
+
+
+    selection.each(function(data) {
+      svg = d3.select(this).append("select")
+      var options = svg.selectAll("option").data(data)
+      options.enter().append("option")
+        .attr("value", function(d) { return d; })
+        .attr("subcategory", function(d) { return d; })
+        .text(function(d) { return d; });
+    });
+  }
+
+  // Getters and Setters
+  my.width = function(value) {
+    if (!arguments.length) { return width; }
+    width = value;
+    return my;
+  };
+
+  my.height = function(value) {
+    if (!arguments.length) { return height; }
+    height = value;
+    return my;
+  };
+
+  my.svg = function() {
+    return svg;
+  };
+
+  return my;
+};
+
+/*jslint browser: true*/
+/*global $, jQuery, d3, _*/
+
+if (d3.charts === null || typeof(d3.charts) !== "object") { d3.charts = {}; }
+
+// Based on http://bost.ocks.org/mike/chart/
 this.d3.charts.groupStack = function() {
   'use strict';
   function my(selection) {
