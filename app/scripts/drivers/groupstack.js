4d9b6d7c1e3b1dd72c320d794cc10e72dc6bdff2
Thomas Newton <thomas_newton@mckinsey.com>
2013-11-25 14:49:29 -0500

    adding svg container

--- app/scripts/drivers/groupstack.js
+++ app/scripts/drivers/groupstack.js
@@ -11,5 +11,6 @@ d3.csv("data/groupstack.csv", function(error, data) {
   groupStack.vertical(true);
   d3.select("#groupStack_vertical").datum(layers).call(groupStack);
   d3.select("#groupStack_vertical_empty").datum(undefined).call(groupStack);
+  console.log(groupStack.svg());
 });
 
